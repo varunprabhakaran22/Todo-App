064992643201c7331e4c505bef8233562077c120
Varun Prabhakaran <50875768+varunprabhakaran22@users.noreply.github.com>
2021-07-30 00:44:48 +0530

    Fix the timer bug

--- client/env/prod_env.sh
+++ client/env/prod_env.sh
@@ -1,3 +1,3 @@
-export REACT_APP_G_HOME_URL=https://www.todo.in/
-export REACT_APP_G_URL=https://www.todo.in/
+export REACT_APP_G_HOME_URL=https://modest-johnson-aeaed6.netlify.app/
+export REACT_APP_G_URL=https://modest-johnson-aeaed6.netlify.app/
 export PORT=7080
